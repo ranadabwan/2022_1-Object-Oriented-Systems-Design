5df412481dce28e81e9f71398217ccd9b3dcad3e
Rana Dabwan <129721504+ranadabwan@users.noreply.github.com>
2023-04-20 13:35:47 +0900

    Print your Name & Student ID.

---
+++ week1/Main.java
@@ -0,0 +1,13 @@
+package Week1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+                String name = "RANA DABWAN";
+                int studentID = 2021034184;
+
+                System.out.println("My name is " + name+ "\n" + "and my student ID is " + studentID );
+
+            }
+        }
